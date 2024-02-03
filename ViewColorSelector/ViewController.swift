539bbf83e85a6d12ab17ec47847b2d478a4f443f
Carlos García Pérez <carlosziok@gmail.com>
2024-02-03 16:54:05 +0300

    Added Code

--- ViewColorSelector/ViewController.swift
+++ ViewColorSelector/ViewController.swift
@@ -7,24 +7,64 @@
 
 import UIKit
 
-private class ViewController: UIViewController {
+class ViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
+    
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var greenValue: UILabel!
+    @IBOutlet weak var blueValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupRedValue()
+        setupGreenValue()
+        setupBlueValue()
+        
+        setupColorView()
     }
 
-    @IBAction func redColorSlide(_ sender: UISlider) {
+    @IBAction func redSliderAction() {
+        setupColorView()
+        redValue.text = redSlider.value.formatted()
     }
-    @IBAction func greenColorSlide(_ sender: UISlider) {
+    @IBAction func greenSliderAction() {
+        setupColorView()
+        greenValue.text = greenSlider.value.formatted()
     }
-    @IBAction func blueColorSlide(_ sender: UISlider) {
+    @IBAction func blueSliderAction() {
+        setupColorView()
+        blueValue.text = blueSlider.value.formatted()
     }
     
+    private func setupRedValue(){
+        redValue.text = redSlider.value.formatted()
+    }
+    private func setupGreenValue(){
+        greenValue.text = greenSlider.value.formatted()
+    }
+    private func setupBlueValue(){
+        blueValue.text = blueSlider.value.formatted()
+    }
+    
+    
+    
+    private func setupColorView() {
+            let red = CGFloat(redSlider.value)
+            let green = CGFloat(greenSlider.value)
+            let blue = CGFloat(blueSlider.value)
+            
+            colorView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+        }
 }
 
+extension Float {
+    func formatted() -> String {
+        return String(format: "%.2f", self)
+    }
+}
