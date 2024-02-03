d7793279e96bb45e6d56a8596f8cd0c246f3cdcf
Carlos García Pérez <carlosziok@gmail.com>
2024-02-03 21:09:34 +0300

    Corrections

--- ViewColorSelector/ViewController.swift
+++ ViewColorSelector/ViewController.swift
@@ -8,24 +8,21 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
-    @IBOutlet weak var colorView: UIView!
     
+    @IBOutlet weak var colorView: UIView!
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
-    
     @IBOutlet weak var redValue: UILabel!
     @IBOutlet weak var greenValue: UILabel!
     @IBOutlet weak var blueValue: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupSlidersValue()
         setupColorView()
     }
-
+    
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         switch sender {
             case redSlider:
@@ -39,20 +36,21 @@ final class ViewController: UIViewController {
         }
         setupColorView()
     }
- 
+    
     private func setupSlidersValue(){
         redValue.text = redSlider.value.formatted()
         greenValue.text = greenSlider.value.formatted()
         blueValue.text = blueSlider.value.formatted()
     }
+    
     private func setupColorView() {
-            let red = CGFloat(redSlider.value)
-            let green = CGFloat(greenSlider.value)
-            let blue = CGFloat(blueSlider.value)
-            
-            colorView.layer.cornerRadius = 16
-            colorView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
-        }
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        colorView.layer.cornerRadius = 16
+        colorView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 }
 
 extension Float {
